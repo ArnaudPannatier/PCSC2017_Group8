afd3b6191628293ea0f0ded4fe05959c2c2e2db8
pcsc <pcsc@pcsc.epfl.ch>
2017-12-08 02:39:34 -0500

    adding reader class

--- inputOutput.cpp
+++ inputOutput.cpp
@@ -1,5 +1,59 @@
 //
-// Created by pcsc on 12/4/17.
+// Created by Haziq on 12/4/17.
 //
 
 #include "inputOutput.h"
+
+//=================================================================================================
+Matrix inputOutput::readFromText(string filename) {
+
+    // Read from text file
+    string line;
+
+    vector2D data;
+    ifstream infile(filename);
+
+    //
+    while (infile)
+    {
+        string s;
+        if (!getline( infile, s )) break;
+
+        istringstream ss( s );
+        vector<double> record;
+
+        // break the current line into separate strings, separated by ','
+        while (ss)
+        {
+            string s;
+            if (!getline( ss, s, ',' )) break;
+            record.push_back( stof(s) );
+        }
+
+        data.push_back( record );
+
+        // each row in the text file must have the same number of entries
+        // cout << record.size() << " " << data[0].size() << endl;
+
+        if(record.size() != data[0].size()) {
+            cerr << filename + " has missing entries. Inserting zeros" << endl;
+        }
+    }
+
+    return Matrix(data);
+
+}
+
+//=================================================================================================
+void inputOutput::writeToText(Matrix data, string filename) {
+
+    ofstream outFile;
+    outFile.open(filename);
+
+    for(size_t row = 0; row < data.size().lines; row++){
+        for(size_t col = 0; col < data.size().cols; col++){
+            outFile << data[row][col];
+        }
+    }
+
+}
--- inputOutput.h
+++ inputOutput.h
@@ -5,12 +5,34 @@
 #ifndef PROJECT_INPUTOUTPUT_H
 #define PROJECT_INPUTOUTPUT_H
 
-
+#include "Matrix.h"
+#include "Vector.h"
+#include <fstream>
+#include <sstream>
 
 class inputOutput {
 
-};
+public:
 
+    /** \brief Reads a matrix from a text file
+      * \param filename either relative or absolute path to the text file
+      * \return a matrix
+      *
+      * This method reads a matrix from a text file. Note that column entries must be separated by commas and row
+      * entries separated by a newline. The method does not
+      *
+      * @code
+      * inputOutput io;
+      * Matrix A = io.readFromText("A.txt");
+      * Matrix b = io.readFromText("b.txt");
+      * @endcode
+      */
+
+    Matrix readFromText(string filename);
+
+    void writeToText(Matrix data, string filename);
+
+};
 
 
 #endif //PROJECT_INPUTOUTPUT_H
