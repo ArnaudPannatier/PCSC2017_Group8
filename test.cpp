1324722f55874442b45ce9f57ac3965f740bcfbb
pcsc <pcsc@pcsc.epfl.ch>
2017-12-08 03:25:46 -0500

    update test file

--- test.cpp
+++ test.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 #include <vector>
 
-#include "utils.h"
+#include "inputOutput.h"
 #include "directSolvers.h"
 #include "iterativeSolvers.h"
 
@@ -21,27 +21,35 @@ int main(){
 
     // ============================================================
     // Test for read text
-    Matrix A = readFromText("A.txt");
-    Matrix b = readFromText("b.txt");
+
+    inputOutput io;
+    Matrix A = io.readFromText("A.txt");
+    Matrix b = io.readFromText("b.txt");
     cout << "A: " << A << endl << "b: " << b << endl;
 
     // ============================================================
+    // Test for solvers
+    // todo: non square matrices, unequal row for A and b
+    // todo: matmul - return scalar when number of elements drop to 1
+    // todo: 1d matrix = always row
+
     // Test for LU factorization
-    // todo: non square matrices, infinity
+    // todo: division by zero
 
     directSolvers dSolvers(A, b);
     dSolvers.LU();
 
     Matrix L = dSolvers.getL();
     Matrix U = dSolvers.getU();
-    //Matrix x = dSolvers.getX();
+    Matrix x = dSolvers.getX();
 
     cout << "L: " << L << endl;
     cout << "U: " << U << endl;
     cout << "LU: " << L*U << endl;
+    cout << "X: " << x << endl;
 
     // ============================================================
-    // Test for conjugate gradient descent
+    // Test for iterative solvers
     // todo: non symmetric, psd
 
     Matrix A2({{4,1},{1,3}});
@@ -51,9 +59,13 @@ int main(){
     b2 = b2.T();
     x_0 = x_0.T();
 
+    // initialize iterative solver
     iterativeSolvers itSolvers(A2, b2);
 
+    // conjugate gradient descent
     cout << "x: " << itSolvers.conjugateGradientDescent(x_0) << endl;
+
+    // gauss seidel
     cout << "x: " << itSolvers.gaussSeidel(x_0) << endl;
 
     return 0;
